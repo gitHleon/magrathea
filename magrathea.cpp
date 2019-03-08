11b3c2a116f6bd500cafc0d5d273f5baa3138d94
dmadaffa <danaffari@gmail.com>
2019-03-08 14:51:47 +0100

    minor fix to camera angle evaluation

--- magrathea.cpp
+++ magrathea.cpp
@@ -341,7 +341,7 @@ void Magrathea::updatePosition(){
     ui->EnableButton_U->setText((current ? "Disable" : "Enable"));
     //reading fault state for each axis
 
-    //return;
+    return;
 
     unsigned int mask1 = ((1 << 1) - 1 ) << 5;//Mask for Software Right Limit
     unsigned int mask2 = ((1 << 1) - 1 ) << 6;//Mask for Software Left  Limit
@@ -892,7 +892,7 @@ bool Magrathea::FiducialFinderCaller(const int &input, std::vector <double> & F_
 
 #if VALENCIA
     double camera_angle = 1.265;
-    double target_x_short = distance_x*0.001*cos(camera_angle) + distance_y*0.001*sin(camera_angle);
+    double target_x_short = - distance_x*0.001*cos(camera_angle) - distance_y*0.001*sin(camera_angle);
     double target_y_short = distance_x*0.001*sin(camera_angle) - distance_y*0.001*cos(camera_angle);
     ofs<<" "<<pos_t[0]-target_x_short<<" "<<pos_t[1]-target_y_short<<" "<<pos_t[4]<<std::endl;
     ofs.close();
@@ -1374,7 +1374,7 @@ bool Magrathea::loop_test(){
                 std::cout<<"FAIL!!"<<std::endl;
                 return false;
             }
-            double camera_angle = -1.265;
+            double camera_angle = 1.265;
             double target_x_short = - distances[0]*cos(camera_angle) - distances[1]*sin(camera_angle);
             double target_y_short = distances[0]*sin(camera_angle) - distances[1]*cos(camera_angle);
             //ATTENTION! distances[0] is cols, distances[1] is rows of the image
@@ -1408,8 +1408,8 @@ bool Magrathea::loop_test_images(){
     //run fiducial finding algo automatically
     cv::destroyAllWindows();
     std::string timestamp = "";
-    std::string address_images = "C:/Users/Silicio/cernbox/Gantry_2018_BIG/Fiducial_chip_images_5_5_20190218/";
-    std::string address_fiducial = "C:/Users/Silicio/cernbox/Gantry_2018_BIG/Templates_5_5/";
+    std::string address_images = "C:/Users/Silicio/cernbox/Gantry_2018_BIG/Fiducial_chip_images_NewOptics_20190306/";
+    std::string address_fiducial = "C:/Users/Silicio/cernbox/Gantry_2018_BIG/Templates_NewOptics/";
     std::string Images_fiducial[] = {
         address_fiducial + "ATLAS_E.jpg",
         address_fiducial + "ATLAS_F.jpg",
@@ -1430,7 +1430,7 @@ bool Magrathea::loop_test_images(){
                 std::string one     = std::to_string(i);
                 std::string two     = std::to_string(j);
                 std::string three   = std::to_string(m);
-                std::string image_address = "Image_"+one+"_"+two+"_"+three+"_.jpg";
+                std::string image_address = "Image_"+one+"_"+two+"_"+three+".jpg";
                 Ffinder->SetImage(address_images + image_address,cv::IMREAD_COLOR);
                 Ffinder->Set_calibration(mCalibration); //get calibration from a private variable
                 cv::Mat output_H;
